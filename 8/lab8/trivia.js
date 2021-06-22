b1180c4488d42043492b87a5aa56ed31b4746e5f
riceset <riceset@icloud.com>
2021-06-22 16:06:39 -0300

    some progress

--- 8/lab8/trivia.js
+++ 8/lab8/trivia.js
@@ -1,16 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+});
+
 function changeColor()
 {
   document.querySelector('#obama').onclick = function() {
     this.style.color = 'red';
-    document.querySelector('#first').innerHTML = "Incorrect";
+    document.querySelector('#answer').innerHTML = "Incorrect";
   }
-  document.querySelector('#trump').onclick = function() {
+  document.querySelector('#answer').onclick = function() {
     this.style.color = 'red';
-    document.querySelector('#first').innerHTML = "Incorrect";
+    document.querySelector('#answer').innerHTML = "Incorrect";
   }
   document.querySelector('#biden').onclick = function() {
     this.style.color = 'green';
-    document.querySelector('#first').innerHTML = "Correct!";
+    document.querySelector('#answer').innerHTML = "Correct!";
   }
 }
 
@@ -21,7 +25,7 @@ function checkAnswer()
 
   document.querySelector('#check').onclick = function() {
     if (answer.toLowerCase() == 'russia') {
-      document.querySelector('#second').innerHTML = "Correct";
+      document.querySelector('#answer2').innerHTML = "Correct";
     }
     else {
       document.querySelector('#second').innerHTML = "Incorrect";
