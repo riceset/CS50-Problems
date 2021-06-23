d084c07e80ca76c950d9daddbeb4c58521042ac8
riceset <riceset@icloud.com>
2021-06-23 18:14:06 -0300

    some progress

---
+++ 8/pset/logic.js
@@ -0,0 +1,17 @@
+//All the greetings
+let greetings = [
+  "Hello World!",
+  "Hola Mundo!",
+  "ハローワールド！",
+  "Bonjour le monde!",
+  "你好世界!",
+];
+
+alert(greetings[1]);
+let counter = 0;
+
+// window.setInterval(function() {
+//   greeting.innerHTML = greetings[counter];
+//   counter++;
+//   counter = counter == lenght ? counter = 0 : counter = counter;
+// }, 3000);
