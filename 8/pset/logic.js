84b0c3d2b48605fc153141c3d592541b8b2a620b
riceset <riceset@icloud.com>
2021-06-23 20:58:31 -0300

    some progress

--- 8/pset/logic.js
+++ 8/pset/logic.js
@@ -1,19 +1,17 @@
 var i = 0;
 
 function changeLang() {
-
   let body = document.querySelector('header');
-
   let greetings = [
     "Hola Mundo!",
     "Bonjour le Monde!",
-    "Hello World!"
+    "Hello World!",
+    "Ciao mondo!",
   ];
-
   body.innerHTML = greetings[i];
-
   i++;
-
   if (i == greetings.length)
     i = 0;
 }
+
+window.setInterval(changeLang, 700);
