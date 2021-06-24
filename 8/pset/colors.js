127377bc8dd731efc749c10ee78ea87d976bacf1
riceset <riceset@icloud.com>
2021-06-24 19:43:35 -0300

    colors with names

--- 8/pset/colors.js
+++ 8/pset/colors.js
@@ -1,20 +1,31 @@
 var i = 0;
+var j = 0;
 
 function changeColor() {
   let body = document.querySelector('body');
-
   let colors = [
     '#ff0000',
     '#00ff00',
-    '#0000ff'
+    '#0000ff',
   ];
-
   body.style.backgroundColor = colors[i];
-
   i++;
-
   if (i == colors.length)
     i = 0;
 }
 
-window.setInterval(changeColor, 500);
+function changeText() {
+  let text = document.querySelector('#text');
+  let colors = [
+    "Red",
+    "Green",
+    "Blue",
+  ];
+  text.innerHTML = colors[j];
+  j++;
+  if (j == colors.length)
+    j = 0;
+}
+
+window.setInterval(changeColor, 700);
+window.setInterval(changeText, 700);
